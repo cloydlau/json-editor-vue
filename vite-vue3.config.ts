917ef85238c4a7aba8e625dcd5edf353006fba8f
cloydlau <253055734@qq.com>
2022-04-21 02:21:26 +0800

    release: v0.4.1

--- vite-vue3.config.ts
+++ vite-vue3.config.ts
@@ -1,13 +1,25 @@
 import type { UserConfigExport, ConfigEnv } from 'vite'
 import { name } from './package.json'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default ({ command }: ConfigEnv): UserConfigExport => {
   return {
+    server: {
+      port: 3003
+    },
     optimizeDeps: {
       exclude: ['vue-demi']
     },
-    plugins: [],
+    plugins: [
+      vue(),
+      {
+        name: 'html-transform',
+        transformIndexHtml (html: string) {
+          return html.replace(/{{.*}}/, `/demo/vue3/index.ts`)
+        },
+      },
+    ],
     build: {
       lib: {
         name,
