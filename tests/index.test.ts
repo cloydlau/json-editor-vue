5a1d45274d59f46931cb2cd2f0263afd538f2d17
cloydlau <253055734@qq.com>
2022-10-21 10:07:45 +0800

    test: add test cases

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,12 +2,21 @@ import { mount } from '@vue/test-utils'
 import { expect, test } from 'vitest'
 import JsonEditorVue from '../src/index'
 
-test('set value', async () => {
+test('string value in text mode', async () => {
   const wrapper = mount(JsonEditorVue, {
     props: {
       mode: 'text',
-      modelValue: 'initialValue',
+      modelValue: 'abc',
     },
   })
-  expect(wrapper.get('.cm-activeLine').text()).toEqual('initialValue')
+  expect(wrapper.get('.cm-activeLine').text()).toEqual('"abc"')
+})
+
+test('string value in tree mode', async () => {
+  const wrapper = mount(JsonEditorVue, {
+    props: {
+      modelValue: 'abc',
+    },
+  })
+  expect(wrapper.get('.jse-value').text()).toEqual('abc')
 })
