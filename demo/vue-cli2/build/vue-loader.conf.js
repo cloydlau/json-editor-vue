768fda6b8770b7738297c2454ddebc177897675e
cloydlau <253055734@qq.com>
2022-12-28 17:47:08 +0800

    test: test compatibility with Vue CLI 2

---
+++ demo/vue-cli2/build/vue-loader.conf.js
@@ -0,0 +1,22 @@
+'use strict'
+const config = require('../config')
+const utils = require('./utils')
+const isProduction = process.env.NODE_ENV === 'production'
+const sourceMapEnabled = isProduction
+  ? config.build.productionSourceMap
+  : config.dev.cssSourceMap
+
+module.exports = {
+  loaders: utils.cssLoaders({
+    sourceMap: sourceMapEnabled,
+    extract: isProduction,
+  }),
+  cssSourceMap: sourceMapEnabled,
+  cacheBusting: config.dev.cacheBusting,
+  transformToRequire: {
+    video: ['src', 'poster'],
+    source: 'src',
+    img: 'src',
+    image: 'xlink:href',
+  },
+}
