a4589996c995658431edb631b40be333486f4a0b
cloydlau <253055734@qq.com>
2023-01-06 15:10:30 +0800

    workflow: modify commitlint rules

--- .commitlintrc.ts
+++ .commitlintrc.ts
@@ -1,7 +1,9 @@
+// Default Config See https://github.com/conventional-changelog/commitlint/tree/master/@commitlint/config-conventional
 const { rules } = require('@commitlint/config-conventional')
 
 // See https://github.com/vuejs/core/blob/main/scripts/verifyCommit.js
 rules['type-enum'][2].push('wip', 'types', 'release', 'workflow', 'dx')
+rules['header-max-length'][2] = 150
 
 module.exports = {
   extends: ['@commitlint/config-conventional'],
