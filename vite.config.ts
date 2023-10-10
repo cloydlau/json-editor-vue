1b1285c567c9fc659c9736178c0dd9656b8e2b47
Cloyd Lau <253055734@qq.com>
2023-10-10 23:41:19 +0800

    chore: omitted

--- vite.config.ts
+++ vite.config.ts
@@ -37,7 +37,7 @@ export default {
     {
       name: 'html-transform',
       transformIndexHtml(html: string) {
-        return html.replace(/\{\{NAME\}\}/, name).replace(/\{\{VUE_VERSION\}\}/g, String(major === 3 ? major : `${major}.${minor}`))
+        return html.replace(/\{\{ NAME \}\}/, name).replace(/\{\{ VUE_VERSION \}\}/g, String(major === 3 ? major : `${major}.${minor}`))
       },
     },
     dts({ rollupTypes: true }),
