051a5aaf45c0f9b5f75a9244521aa9a738dbeb11
Cloyd Lau <31238760+cloydlau@users.noreply.github.com>
2024-03-27 14:44:28 +0800

    chore: modify ESLint rules

--- eslint.config.ts
+++ eslint.config.ts
@@ -12,7 +12,7 @@ export default antfu(
       'style/brace-style': ['error', '1tbs', { allowSingleLine: true }],
       'vue/attribute-hyphenation': 'off',
       'vue/custom-event-name-casing': 'off',
-      'vue/max-attributes-per-line': ['error', { singleline: 1, multiline: 1 }],
+      'vue/max-attributes-per-line': 'error',
       'vue/no-deprecated-v-bind-sync': 'off',
       'vue/no-deprecated-v-on-native-modifier': 'off',
       'vue/no-deprecated-destroyed-lifecycle': 'off',
