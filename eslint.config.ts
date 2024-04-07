f755ccfc19f22a07447a1cda3551afad0a38595f
Cloyd Lau <31238760+cloydlau@users.noreply.github.com>
2024-04-07 11:12:46 +0800

    chore: modify ESLint rules

--- eslint.config.ts
+++ eslint.config.ts
@@ -10,7 +10,6 @@ export default antfu(
       'curly': ['error', 'all'],
       'no-console': 'off',
       'vue/attribute-hyphenation': 'off',
-      'vue/custom-event-name-casing': 'off',
       'vue/max-attributes-per-line': 'error',
       'vue/multi-word-component-names': 'off',
       'vue/no-deprecated-v-bind-sync': 'off',
