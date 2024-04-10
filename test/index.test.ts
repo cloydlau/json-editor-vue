a12a12839cc04bc1b03a3f46167dc51eb40318ce
Cloyd Lau <31238760+cloydlau@users.noreply.github.com>
2024-04-10 09:49:35 +0800

    release: v0.13.2

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,24 +3,43 @@ import { mount } from '@vue/test-utils'
 import { expect, it } from 'vitest'
 import JsonEditorVue from '../src/index'
 
-it('string value in text mode', async () => {
+it('text mode', async () => {
   const wrapper = mount(JsonEditorVue, {
     props: {
       mode: 'text',
       modelValue: '',
     },
   })
-  await wrapper.setProps({ modelValue: 'abc' })
-  expect(wrapper.vm.jsonEditor.get().json).toEqual('abc')
+
+  await wrapper.setProps({ modelValue: '123' })
+  await nextTick()
+  expect(wrapper.vm.jsonEditor.get().text).toEqual('123')
+
+  await wrapper.setProps({ modelValue: JSON.stringify({ a: 1 }) })
+  await nextTick()
+  expect(wrapper.vm.jsonEditor.get().text).toEqual(JSON.stringify({ a: 1 }))
+
+  await wrapper.setProps({ modelValue: { a: 1 } })
+  await nextTick()
+  expect(wrapper.vm.jsonEditor.get().json).toEqual({ a: 1 })
 })
 
-it('string value in tree mode', async () => {
+it('tree mode', async () => {
   const wrapper = mount(JsonEditorVue, {
     props: {
       modelValue: null,
     },
   })
+
+  await wrapper.setProps({ modelValue: '123' })
+  await nextTick()
+  expect(wrapper.vm.jsonEditor.get().text).toEqual('123')
+
   await wrapper.setProps({ modelValue: JSON.stringify({ a: 1 }) })
   await nextTick()
-  expect(wrapper.vm.jsonEditor.get().json).toEqual(JSON.stringify({ a: 1 }))
+  expect(wrapper.vm.jsonEditor.get().text).toEqual(JSON.stringify({ a: 1 }))
+
+  await wrapper.setProps({ modelValue: { a: 1 } })
+  await nextTick()
+  expect(wrapper.vm.jsonEditor.get().json).toEqual({ a: 1 })
 })
